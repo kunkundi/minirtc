a09fbd0be41b2538e1cc59952f4d5ea56b96863b
dijunkun <junkun.di@hotmail.com>
2025-05-22 17:59:55 +0800

    [fix] remove non-const data() for CopyOnWriteBuffer

--- src/common/copy_on_write_buffer.h
+++ src/common/copy_on_write_buffer.h
@@ -40,11 +40,6 @@ class CopyOnWriteBuffer {
 
   const uint8_t* data() const { return block_ ? block_->data : nullptr; }
 
-  uint8_t* data() {
-    EnsureUnique();
-    return block_ ? block_->data : nullptr;
-  }
-
   size_t size() const { return block_ ? block_->size : 0; }
 
   size_t capacity() const { return block_ ? block_->capacity : 0; }
