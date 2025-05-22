ca1c7d8a1e25af8eaff0828f3e7bfd929bb4e1cc
dijunkun <junkun.di@hotmail.com>
2025-05-22 17:11:17 +0800

    [feat] execute time log enabled for TaskQueue

--- src/common/task_queue.h
+++ src/common/task_queue.h
@@ -1,49 +1,34 @@
-/*
- * @Author: DI JUNKUN
- * @Date: 2025-03-14
- * Copyright (c) 2025 by DI JUNKUN, All Rights Reserved.
- */
-
 #ifndef _TASK_QUEUE_H_
 #define _TASK_QUEUE_H_
 
+#include <atomic>
 #include <chrono>
 #include <condition_variable>
 #include <functional>
+#include <iostream>
 #include <memory>
 #include <mutex>
 #include <queue>
+#include <string>
 #include <thread>
 #include <vector>
 
 #include "any_invocable.h"
+#include "log.h"
 
 class TaskQueue {
  public:
-  TaskQueue(std::string task_name, size_t numThreads = 1)
-      : task_name_(task_name),
-        stop_(false),
-        workers_(),
-        taskQueue_(),
-        mutex_(),
-        cond_var_() {
-    for (size_t i = 0; i < numThreads; i++) {
+  TaskQueue(std::string task_name, bool log_enabled = false,
+            size_t numThreads = 1)
+      : task_name_(std::move(task_name)),
+        log_enabled_(log_enabled),
+        stop_(false) {
+    for (size_t i = 0; i < numThreads; ++i) {
       workers_.emplace_back([this]() { this->WorkerThread(); });
     }
   }
 
-  ~TaskQueue() {
-    {
-      std::unique_lock<std::mutex> lock(mutex_);
-      stop_ = true;
-    }
-    cond_var_.notify_all();
-    for (std::thread &worker : workers_) {
-      if (worker.joinable()) {
-        worker.join();
-      }
-    }
-  }
+  ~TaskQueue() { Stop(); }
 
   void PostTask(AnyInvocable<void()> task) {
     PostDelayedTask(std::move(task), 0);
@@ -52,75 +37,128 @@ class TaskQueue {
   void PostDelayedTask(AnyInvocable<void()> task, int delay_ms) {
     auto execute_time =
         std::chrono::steady_clock::now() + std::chrono::milliseconds(delay_ms);
+
+    bool notify = false;
     {
       std::unique_lock<std::mutex> lock(mutex_);
+      if (taskQueue_.empty() || execute_time < taskQueue_.top().execute_time) {
+        notify = true;
+      }
       taskQueue_.emplace(execute_time, std::move(task));
     }
-    cond_var_.notify_one();
+    if (notify) {
+      cond_var_.notify_one();
+    }
   }
 
   void ClearTasks() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (!taskQueue_.empty()) {
+      taskQueue_.pop();
+    }
+  }
+
+  void Stop() {
     {
       std::unique_lock<std::mutex> lock(mutex_);
       stop_ = true;
-      while (!taskQueue_.empty()) {
-        taskQueue_.pop();
-      }
     }
     cond_var_.notify_all();
-    for (std::thread &worker : workers_) {
+    for (std::thread& worker : workers_) {
       if (worker.joinable()) {
         worker.join();
       }
     }
+    workers_.clear();
   }
 
  private:
   struct TaskItem {
+    std::chrono::steady_clock::time_point enqueue_time;
     std::chrono::steady_clock::time_point execute_time;
     AnyInvocable<void()> task = nullptr;
 
-    TaskItem(std::chrono::steady_clock::time_point time,
+    TaskItem(std::chrono::steady_clock::time_point execute_time_,
              AnyInvocable<void()> func)
-        : execute_time(time), task(std::move(func)) {}
+        : enqueue_time(std::chrono::steady_clock::now()),
+          execute_time(execute_time_),
+          task(std::move(func)) {}
 
-    bool operator>(const TaskItem &other) const {
+    bool operator>(const TaskItem& other) const {
       return execute_time > other.execute_time;
     }
   };
 
   void WorkerThread() {
     while (true) {
-      AnyInvocable<void()> task;
+      TaskItem task_item(std::chrono::steady_clock::now(), nullptr);
+
       {
         std::unique_lock<std::mutex> lock(mutex_);
-        cond_var_.wait(lock, [this]() { return !taskQueue_.empty() || stop_; });
+        cond_var_.wait(lock, [this]() { return stop_ || !taskQueue_.empty(); });
 
         if (stop_ && taskQueue_.empty()) return;
 
-        auto now = std::chrono::steady_clock::now();
-        if (taskQueue_.top().execute_time > now) {
-          cond_var_.wait_until(lock, taskQueue_.top().execute_time,
-                               [this]() { return stop_; });
+        while (!taskQueue_.empty()) {
+          if (log_enabled_) {
+            LOG_INFO("[TaskQueue: {}] size {}", task_name_.c_str(),
+                     taskQueue_.size());
+          }
+          auto now = std::chrono::steady_clock::now();
+          const auto& top = taskQueue_.top();
+
+          if (top.execute_time > now) {
+            cond_var_.wait_until(lock, top.execute_time, [this, now]() {
+              return stop_ || (!taskQueue_.empty() &&
+                               taskQueue_.top().execute_time <=
+                                   std::chrono::steady_clock::now());
+            });
+            if (stop_) return;
+            continue;
+          }
+
+          task_item = std::move(const_cast<TaskItem&>(taskQueue_.top()));
+          taskQueue_.pop();
+          break;
         }
-
-        if (stop_ && taskQueue_.empty()) return;
-
-        task = std::move(
-            const_cast<AnyInvocable<void()> &>(taskQueue_.top().task));
-        taskQueue_.pop();
       }
-      task();
+
+      if (!task_item.task) {
+        continue;
+      }
+
+      if (log_enabled_) {
+        auto start_exec = std::chrono::steady_clock::now();
+        auto queue_duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(
+                start_exec - task_item.enqueue_time);
+
+        LOG_INFO("[TaskQueue: {}] Task queued for {} ms", task_name_.c_str(),
+                 queue_duration.count());
+
+        auto exec_begin = std::chrono::steady_clock::now();
+        task_item.task();
+        auto exec_end = std::chrono::steady_clock::now();
+        auto exec_duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(exec_end -
+                                                                  exec_begin);
+
+        LOG_INFO("[TaskQueue: {}] Task executed in {} ms", task_name_,
+                 exec_duration.count());
+      } else {
+        task_item.task();
+      }
     }
   }
 
   std::string task_name_;
+  bool log_enabled_ = false;
   std::vector<std::thread> workers_;
   std::priority_queue<TaskItem, std::vector<TaskItem>, std::greater<>>
       taskQueue_;
-  mutable std::mutex mutex_;
+  std::mutex mutex_;
   std::condition_variable cond_var_;
   bool stop_;
 };
 
-#endif
+#endif  // _TASK_QUEUE_H_
