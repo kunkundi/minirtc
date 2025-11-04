ec7601c2e97643a15c2c0a9f5216268fd452496f
dijunkun <junkun.di@hotmail.com>
2025-11-04 23:50:11 +0800

    [feat] use trickle ICE for DataChannelConnection

--- src/dc/datachannel_transport.cpp
+++ src/dc/datachannel_transport.cpp
@@ -36,76 +36,80 @@ DataChannelTransport::~DataChannelTransport() {
 
 int DataChannelTransport::SendVideoFrame(const XVideoFrame* video_frame,
                                          const std::string& stream_id) {
-  for (auto& it : video_streams_) {
-    if (it.first == stream_id) {
-      auto& track = it.second->track_;
-      auto& codec = it.second->codec_;
+  if (video_streams_.find(stream_id) == video_streams_.end()) {
+    LOG_ERROR("Video stream [{}] not found", stream_id);
+    return -1;
+  }
 
-      if (!codec) {
-        LoadNvCodecDll();
-        codec = VideoEncoderFactory::CreateVideoEncoder(clock_, false, false);
-        if (!codec || 0 != codec->Init()) {
-          LOG_ERROR("Encoder [{}] init failed", stream_id);
-          return -1;
-        }
-      }
+  auto& stream = video_streams_[stream_id];
+  auto& track = stream->track_;
+  auto& codec = stream->codec_;
 
-      if (!task_queue_encode_) {
-        LOG_ERROR("Encoder task queue not init");
-        return -1;
-      }
-
-      RawFrame raw_frame((const uint8_t*)video_frame->data, video_frame->size,
-                         video_frame->width, video_frame->height);
-      raw_frame.SetCapturedTimestamp(clock_->CurrentTimeUs());
-
-      std::weak_ptr<DataChannelTransport> weak_this = shared_from_this();
-      auto track_ptr = track;
-      auto codec_ptr = codec;
-
-      task_queue_encode_->PostTask([weak_this, raw_frame = std::move(raw_frame),
-                                    stream_id, track_ptr, codec_ptr]() mutable {
-        auto self = weak_this.lock();
-        if (!self) {
-          LOG_ERROR("[{}] DataChannelTransport is released", self->local_id_);
-          return -1;
-        }
-
-        if (track_ptr && track_ptr->isClosed()) {
-          LOG_ERROR("[{}] Track is closed, drop raw frame size {}",
-                    self->local_id_, raw_frame.Size());
-          return -1;
-        }
-
-        int ret = codec_ptr->Encode(
-            std::move(raw_frame),
-            [weak_self = weak_this, stream_id,
-             track_ptr](const EncodedFrame& encoded_frame) -> int {
-              auto self2 = weak_self.lock();
-              if (!self2) {
-                LOG_ERROR("DataChannelTransport is released");
-                return -1;
-              }
-              if (!track_ptr || !track_ptr->isOpen()) {
-                LOG_ERROR("Track is closed, drop encoded frame size");
-                return -1;
-              }
-
-              track_ptr->sendFrame(
-                  reinterpret_cast<const std::byte*>(encoded_frame.Buffer()),
-                  encoded_frame.Size(),
-                  std::chrono::duration<double, std::micro>(
-                      encoded_frame.EncodedTimestamp()));
-              LOG_ERROR("Send video frame size {}", encoded_frame.Size());
-
-              return 0;
-            });
-        return 0;
-      });
-
-      return 0;
+  if (!codec) {
+    LoadNvCodecDll();
+    codec = VideoEncoderFactory::CreateVideoEncoder(clock_, false, false);
+    if (!codec || 0 != codec->Init()) {
+      LOG_ERROR("Encoder [{}] init failed", stream_id);
+      return -1;
     }
   }
+
+  if (!task_queue_encode_) {
+    LOG_ERROR("Encoder task queue not init");
+    return -1;
+  }
+
+  RawFrame raw_frame((const uint8_t*)video_frame->data, video_frame->size,
+                     video_frame->width, video_frame->height);
+  raw_frame.SetCapturedTimestamp(clock_->CurrentTimeUs());
+
+  std::weak_ptr<DataChannelTransport> weak_this = shared_from_this();
+  auto track_ptr = track;
+  auto codec_ptr = codec;
+
+  task_queue_encode_->PostTask([weak_this, raw_frame = std::move(raw_frame),
+                                stream_id, track_ptr, codec_ptr]() mutable {
+    auto self = weak_this.lock();
+    if (!self) {
+      LOG_ERROR("[{}] DataChannelTransport is released", self->local_id_);
+      return -1;
+    }
+
+    if (track_ptr && track_ptr->isClosed()) {
+      LOG_ERROR("[{}] Track is closed, drop raw frame size {}", self->local_id_,
+                raw_frame.Size());
+      return -1;
+    }
+
+    int ret = codec_ptr->Encode(
+        std::move(raw_frame),
+        [weak_self = weak_this, stream_id,
+         track_ptr](const EncodedFrame& encoded_frame) -> int {
+          auto self2 = weak_self.lock();
+          if (!self2) {
+            LOG_ERROR("DataChannelTransport is released");
+            return -1;
+          }
+          if (!track_ptr || !track_ptr->isOpen()) {
+            LOG_ERROR("Track is closed, drop encoded frame size {}",
+                      encoded_frame.Size());
+            return -1;
+          }
+
+          track_ptr->sendFrame(
+              reinterpret_cast<const std::byte*>(encoded_frame.Buffer()),
+              encoded_frame.Size(),
+              std::chrono::duration<double, std::micro>(
+                  encoded_frame.EncodedTimestamp()));
+          LOG_ERROR("Send video frame size {}", encoded_frame.Size());
+
+          return 0;
+        });
+    return 0;
+  });
+
+  return 0;
+
   return -1;
 }
 
