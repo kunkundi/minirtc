0481f90226813b27bc0954bc93aa8d6094e88ba8
dijunkun <dijunkun@zhejianglab.com>
2026-07-31 16:40:44 +0800

    [fix] support older AArch64 compilers in libyuv

--- thirdparty/libyuv/xmake.lua
+++ thirdparty/libyuv/xmake.lua
@@ -9,9 +9,50 @@ package("libyuv")
     add_deps("cmake")
 
     on_install("windows", "linux", "macosx", "android", "cross", "bsd", "mingw", function (package)
-        local configs = {"-DTEST=OFF -DENABLE_SSE2=1 -DENABLE_AVX2=1"}
+        local configs = {"-DUNIT_TEST=OFF"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        
+
+        -- Recent libyuv releases unconditionally enable I8MM and SVE2 for
+        -- AArch64. Older compilers (for example GCC 9) reject those -march
+        -- modifiers, so retain the optimized objects only when the compiler
+        -- accepts their flags.
+        io.replace("CMakeLists.txt",
+            "    target_compile_options(${ly_lib_name}_neon64 PRIVATE -march=armv8.2-a+dotprod+i8mm)",
+            [[    include(CheckCXXCompilerFlag)
+    check_cxx_compiler_flag("-march=armv8.2-a+dotprod+i8mm" LIBYUV_CAN_COMPILE_I8MM)
+    if(LIBYUV_CAN_COMPILE_I8MM)
+      target_compile_options(${ly_lib_name}_neon64 PRIVATE -march=armv8.2-a+dotprod+i8mm)
+    else()
+      target_compile_options(${ly_lib_name}_neon64 PRIVATE -march=armv8.2-a+dotprod)
+      target_compile_definitions(${ly_lib_name}_neon64 PRIVATE LIBYUV_NEEDS_I8MM_ASM_DIRECTIVE)
+    endif()]],
+            {plain = true})
+        io.replace("CMakeLists.txt",
+            [[    # Enable AArch64 SVE kernels.
+    add_library(${ly_lib_name}_sve OBJECT
+      ${ly_src_dir}/row_sve.cc)
+    target_compile_options(${ly_lib_name}_sve PRIVATE -march=armv8.5-a+i8mm+sve2)
+    list(APPEND ly_lib_parts $<TARGET_OBJECTS:${ly_lib_name}_sve>)]],
+            [[    # Enable AArch64 SVE kernels when supported by the compiler.
+    check_cxx_compiler_flag("-march=armv8.5-a+i8mm+sve2" LIBYUV_CAN_COMPILE_SVE2)
+    if(LIBYUV_CAN_COMPILE_SVE2)
+      add_library(${ly_lib_name}_sve OBJECT
+        ${ly_src_dir}/row_sve.cc)
+      target_compile_options(${ly_lib_name}_sve PRIVATE -march=armv8.5-a+i8mm+sve2)
+      list(APPEND ly_lib_parts $<TARGET_OBJECTS:${ly_lib_name}_sve>)
+    else()
+      add_definitions(-DLIBYUV_DISABLE_SVE)
+    endif()]],
+            {plain = true})
+        io.replace("source/row_neon64.cc",
+            [[#include "libyuv/row.h"]],
+            [[#include "libyuv/row.h"
+
+#if defined(LIBYUV_NEEDS_I8MM_ASM_DIRECTIVE)
+asm(".arch_extension i8mm");
+#endif]],
+            {plain = true})
+
         io.replace("CMakeLists.txt", "INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert			DESTINATION bin )", "", {plain = true})
         import("package.tools.cmake").install(package, configs)
         
